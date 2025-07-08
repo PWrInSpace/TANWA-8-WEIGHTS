4904d074fc8b262ee6b0016d4c6d2153091e57cf
Bart3kTK <github-actions@github.com>
2025-07-08 19:31:15 +0200

    [maly update]
    cli ma change mux i n raw samples wiec ez

--- components/config/console_config.c
+++ components/config/console_config.c
@@ -18,6 +18,7 @@
 #include "timers_config.h"
 #include "app_task.h"
 #include "sd_task.h"
+#include "ads1256_task.h"
 #define TAG "CONSOLE_CONFIG"
 
 
@@ -60,6 +61,111 @@ int empty_sd_file(int argc, char **argv) {
     }
     return 0;
 }
+
+int change_mux_channel(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        ESP_LOGE(TAG, "One arg needed");
+        return -1;
+    }
+
+    int channel = atoi(argv[1]);
+
+    if(channel > 8 || channel < 1)
+    {
+        ESP_LOGE(TAG, "Channels range is 1-8");
+        return -1;
+    }
+
+    ads1256_device_t dev;
+    uint8_t channel_hex;
+
+    switch(channel)
+    {
+        case 1: channel_hex = MUX_REGISTER_FIRST_CHANNEL; break;
+        case 2: channel_hex = MUX_REGISTER_SECOND_CHANNEL; break;
+        case 3: channel_hex = MUX_REGISTER_THIRD_CHANNEL; break;
+        case 4: channel_hex = MUX_REGISTER_FOURTH_CHANNEL; break;
+        default: ESP_LOGE(TAG, "Unknow channel value"); return -1;
+    }
+
+    if(channel <5)
+    {
+        dev = ADS1256_DEVICE_1;
+    }
+    else
+    {
+        dev = ADS1256_DEVICE_2;
+    }
+
+    if(!ads1256_change_channel(dev, channel_hex))
+    {
+        ESP_LOGE(TAG, "Channel change error");
+        return -1;
+    }
+
+    ESP_LOGI(TAG, "Channel changed!");
+    return 0;
+
+}
+
+int ads1256_get_sampes(int argc, char **argv)
+{
+    if(argc != 3)
+    {
+        ESP_LOGE(TAG, "Usage: command [dev_num] [nr_of_samples]");
+        return -1;
+    }
+
+
+    int device = atoi(argv[1]);
+    int samples = atoi(argv[2]);
+    ads1256_device_t dev;
+
+    if(device == 1)
+    {
+        dev = ADS1256_DEVICE_1;
+    }
+    else if(device == 2)
+    {
+        dev = ADS1256_DEVICE_2;
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Wrong dev_num. 1 - DEV1, 2-DEV2");
+        return -1;
+    }
+
+    if(samples < 1)
+    {
+        ESP_LOGE(TAG, "Number of samples have to be greatcher than 0");
+        return -1;
+    }
+
+    uint8_t data[3];
+    int32_t value;
+
+
+    for(int i =0; i<samples; i++)
+    {
+        if(!ads1256_get_raw_data(dev, data))
+        {
+            return -1;
+        }
+        value = (data[0] << 16) | (data[1] << 8) | data[2];
+
+        if (value & 0x800000) {
+            value |= 0xFF000000;
+        }
+
+        ESP_LOGI(TAG, "Raw signed value: %d", value);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+    return 0;
+}
  // Place for the console configuration
 
  static esp_console_cmd_t cmd [] = {
@@ -70,6 +176,8 @@ int empty_sd_file(int argc, char **argv) {
  {"readc_task_on_15sek", "Start the readc task", NULL, readc_task, NULL},
 {"read_sd_file", "Read a file from the SD card", NULL, read_sd_file, NULL},
 {"empty_sd_file", "Empty a file on the SD card", NULL, empty_sd_file, NULL},
+{"ads_samples", "Returns read data for n sec (int dev, int samples)", NULL,ads1256_get_sampes, NULL },
+{"ads_change_mux", "Mux change command (int mux nr) 1-4 dev1 muxs 5-8 dev2 muxs", NULL, change_mux_channel, NULL},
  };
 
 esp_err_t console_config_init() {
